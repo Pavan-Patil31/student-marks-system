eef98a14394b476ee0b83a684cb07bc63bead130
Pavan-Patil31 <pavandpatil003@gmail.com>
2025-06-16 21:42:03 +0530

    main file changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AppProvider } from './context/AppContext.jsx';
+import { createRoot } from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
